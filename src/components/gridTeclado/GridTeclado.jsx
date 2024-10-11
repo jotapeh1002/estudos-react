725e23ce616bbc360f27683ab8f65a83acdc8503
jotapeh <alisssongomes22@gmail.com>
2024-10-11 18:32:00 -0300

    first commit

--- src/components/gridTeclado/GridTeclado.jsx
+++ src/components/gridTeclado/GridTeclado.jsx
@@ -2,7 +2,7 @@ export function GridTeclado({tecla, width = 'h-[50px]',higth = 'w-[50px]'}){
 
     
     return(
-        <button onClick={} className={`bg-blue-500 p-1 px-2 ${width} ${higth} text-white shadow-sm rounded-lg
+        <button  className={`bg-blue-500 p-1 px-2 ${width} ${higth} text-white shadow-sm rounded-lg
         sm:hover:-translate-y-1 outline-none my-1 ` }> {tecla}</button>
     )
 }
